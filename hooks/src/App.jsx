63f0a1565dc4e6d0abb6166f6554ad1e786380e4
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-23 10:34:48 +0300

    import UserList to main.jsx

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import UserList from './StateWithApi';
+
 
 function App() {
   const [count, setCount] = useState(0)
--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import UserList from './StateWithApi';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <UserList />
   </StrictMode>,
 )
