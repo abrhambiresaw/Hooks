d856c79037244e659f3ef61076d7d399cb6cbc06
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-24 22:43:24 +0300

    import context in component4 and use it

--- hooks/src/useContext/component4.jsx
+++ hooks/src/useContext/component4.jsx
@@ -1,8 +1,14 @@
+import React, { useContext } from "react";
+import { UserContext } from "./component1";
+
 function Component4(){
+
+ const user = useContext(UserContext);
 
   return(
     <div className="box">
       <h1>Component4</h1>
+      <h2>{`Bye ${user}`}</h2>
     </div>
   )
 }
